125c7f4e39ae0f017aebfcf838da31b13b2bad36
a-dykim <d.kim3@columbia.edu>
2018-12-07 20:46:41 -0500

    sum code updated

--- HW4_Code/ml_hw4_p3.py
+++ HW4_Code/ml_hw4_p3.py
@@ -1,87 +1,87 @@
 import numpy as np
 import pandas as pd
+from itertools import permutations 
 
-def compute_distance(coord1, coord2):
+def get_permutation_pairs(length):
     """
-    Given the two coordinate, compute L2 distance
+    Compute all permutation pairs
     """
-    coord1 = np.array(coord1)
-    coord2 = np.array(coord2)
-    distance = np.sqrt(np.sum((coord1 - coord2)**2))
-    return distance
-
-def subtract_wrt_keyword(dic1, dic2):
-    """
-    Subtract dictionary with same keywords but different values
-    """
-    keys= list(dic1.keys())
-    difference = 0
-    for i in range(len(keys)):
-        val1 = np.array(dic1[keys[i]])
-        val2 = np.array(dic2[keys[i]])
-        difference += np.sqrt(np.sum((val1 - val2)**2))
-    return difference # returns to mean difference
-
-def multi_GD_full_rotation(dist_mat, coord, max_totiter=1000, maxiter=1000, learning_rate=0.1, precision=1e-1, change_threshold=0.1):
-    """
-    Run a GD on all 9 cities full rotation
+    arr = np.arange(length)
+    perm = permutations(arr,2)
+    seq = []
+    for i in list(perm):
+        seq.append(i)
     
-    Input:
-    dist_mat : distance matrix (panda Dataframe)
-    coordinates : dictionary format(city:[lat,long])
+    return seq
+    
+def compute_distances(coordinates):
     """
-    cities = ['NYC', 'BOS', 'DC', 'MIA', 'CHI', 'SEA', 'SF', 'LA', 'DEN']
+    Compute L2 distances between all coordinate combinations
+    
+    Input: coordinates dictionary
+    """
+    cities = ['BOS', 'NYC', 'DC', 'MIA', 'CHI', 'SEA', 'SF', 'LA', 'DEN']
+    ncity = len(cities)
+    pairs = get_permutation_pairs(ncity)
+    distance_matrix = np.zeros((ncity, ncity))
+    
+    for i in range(len(pairs)):
+        loc1 = pairs[i][0]
+        loc2 = pairs[i][1]
+        city1 = np.array(coordinates[cities[loc1]])
+        city2 = np.array(coordinates[cities[loc2]])
+        distance_matrix[loc1, loc2] = np.sqrt(np.sum((city1 - city2)**2))
+    
+    return np.array(distance_matrix)
+
+    
+def compute_GD(dist_mat, coord, maxiter=1000, learning_rate=0.001, precision=100):
+    """
+    Compute GD on all cities
+    """
+    cities = ['BOS', 'NYC', 'DC', 'MIA', 'CHI', 'SEA', 'SF', 'LA', 'DEN']
+    ncity = len(cities)
+    dist_new = compute_distances(coord) ## compute all distance matrix 
+    error = np.sqrt(np.sum(dist_mat - dist_new)**2) ## set as the sqrt of discrepancy function
+    now = coord.copy()
+    old = coord.copy()
     niter = 0
-    changes = 1000 # random initial start
-    while changes > change_threshold:
-        if niter == 0:
-            old = coord.copy()
-            new = {}
-            new[cities[0]] = coord[cities[0]]
-
-        for i in range(9):
-            if i == 8:
-                j = 0
-            else:
-                j = i+1
-
-            city1 = cities[i] ## fixed coordinate
-            city2 = cities[j]
-
-            new[city2]= one_GD(int(dist_mat[city1][city2]), old[city1], old[city2], maxiter, learning_rate, precision)
-
-        changes = subtract_wrt_keyword(new, old)
-        old = new.copy()
+    error_new = 1e5
+    while error > precision:
+    #and niter < maxiter:
+        error_old = error_new
+        deltas = {}
+        for i in range(ncity):
+            fix = np.array(coord[cities[i]])
+            #print (cities[i])
+            mini = np.zeros((ncity,2))
+            for j in range(ncity):
+                if i == j:
+                    pass
+                else:
+                    subtract = np.array(coord[cities[j]]) # avoid the subtraction from the same city
+                    mini[j] = (dist_new[i][j] - dist_mat[i][j])/dist_new[i][j] * (fix-subtract) 
+                    
+            deltas[cities[i]] = np.sum(mini, axis=0) # \sum_j (x_i - x_j)
+            #print (deltas)
+        for k in range(ncity):    
+            city = cities[k]
+            now[city] = old[city] - learning_rate * deltas[city]
+            #print (city)
+            #print (now[city])
+            #print (deltas[city])
+        
+        dist_new = compute_distances(now)
+        #print (dist_new[0])
+        error_new = np.sqrt(np.sum((dist_new - dist_mat)**2))
+        old = now
         niter += 1
-    return new
+        print (error_new)
+        if error_new > error_old:
+            return now
+            
 
-def one_GD(dist12, coord1, coord2, maxiter=1000, learning_rate=0.1, precision=1e-2):
-    """
-    Run a gradient descent on two coordinates with respct to coord1
-    """
-    coord1 = np.array(coord1)
-    coord2 = np.array(coord2)
-    initial_dist = compute_distance(coord1, coord2)
-    error = np.abs(initial_dist - dist12)
-    c2_now = coord2.copy()
-    c2_old = coord2.copy()
-    niter = 0
-    while error > precision and niter < maxiter:
-        if niter == 0:
-            grad_disc = (initial_dist - dist12) * (coord2- coord1) / initial_dist
 
-        else:
-            coord_dist = coord_dist = compute_distance(coord1, c2_old)
-            grad_disc = (coord_dist - dist12) * (c2_old- coord1) / coord_dist
-
-        c2_now = c2_old - learning_rate * grad_disc
-
-        error = np.abs(np.sqrt(np.sum((c2_now - coord1)**2)) - dist12)
-        c2_old = c2_now
-        niter +=1
-        #discrepancy = (coord_dist - dist12)**2
-
-    return c2_now
 
 distances = np.array([['', 'BOS', 'NYC', 'DC', 'MIA', 'CHI', 'SEA', 'SF', 'LA', 'DEN'], ['BOS', 0, 206, 429, 1504, 963, 2976, 3095, 2979, 1949], ['NYC', 206, 0, 233, 1308, 902, 2815, 2934, 2786, 1771], ['DC', 429, 344, 0, 1075, 671, 2684, 2799, 2631, 1616], ['MIA', 1504, 1308, 1075, 0, 1329, 3273, 3053, 2687, 2037], ['CHI', 963, 802, 671, 1329, 0, 2013, 2142, 2054, 996], ['SEA', 2976, 2815, 2684, 3273, 2013, 0, 808, 1131, 1307], ['SF', 3095, 2934, 2799, 3053, 2142, 808, 0, 379, 1235], ['LA', 2979, 2786, 2631, 2687, 2054, 1131, 379, 0, 1059], ['DEN', 1949, 1771, 1616, 2037, 996, 1307, 1235, 1059, 0]])
 
@@ -90,10 +90,20 @@ distance_df = pd.DataFrame(data=distances[1:,1:],
                   index=distances[1:,0],
                   columns=distances[0,1:])
 
-init = np.random.random_integers(low=0, high=2000, size=16)
+cities = ['BOS', 'NYC', 'DC', 'MIA', 'CHI', 'SEA', 'SF', 'LA', 'DEN']
+df_mat = np.zeros((9,9))
+for i in range(9):
+    for j in range(9):
+        df_mat[i,j] = int(distance_df[cities[i]][cities[j]])
+    
+
+init = np.random.random_integers(low=-100, high=100, size=16)
 mockcity_dict = {'NYC':[0,0], 'BOS':[init[0], init[1]], 'DC':[init[2], init[3]], 'MIA':[init[4], init[5]],
                  'CHI':[init[6], init[7]], 'SEA':[init[8], init[9]], 'SF':[init[10], init[11]],
                  'LA':[init[12], init[13]], 'DEN':[init[14], init[15]]}
 
-result= multi_GD_full_rotation(distance_df,mockcity_dict, max_totiter=10000, maxiter=1000, learning_rate=0.01, precision=1e-6, change_threshold=1e-6)
+
+result= compute_GD(df_mat,mockcity_dict, learning_rate=0.001, precision=100)
+print (result)
+
 
